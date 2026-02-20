1ed65c1636e855f8d8961a4df54af61e0ccab47d
Benjamin Lesne <benjamin.lesne@outlook.fr>
2026-02-20 09:55:48 +0100

    TP 7 bis?

--- src/gateway.ts
+++ src/gateway.ts
@@ -1,12 +1,17 @@
 import type { Pokemon } from "./database.js";
 import { pokemons } from "./database.js";
 
-export interface PokemonGateway {
-  getAllPokemons(): readonly Pokemon[];
-}
+export type PokemonGateway = typeof pokemonGateway;
 
-export const pokemonGateway: PokemonGateway = {
+export const pokemonGateway = {
   getAllPokemons() {
     return pokemons;
   },
+  getPokemonByType<T extends Pokemon>(type: T["type"]): T {
+    const result = pokemons.filter((pokemon) => pokemon.type === type);
+
+    return result;
+  },
 };
+
+const test = pokemonGateway.getPokemonByType("Fire");
